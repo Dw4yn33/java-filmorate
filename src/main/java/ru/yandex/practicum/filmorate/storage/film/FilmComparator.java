f93ba073a87b35d1359d150ba29a75538b2d9e37
Dw4yn33 <vova-kalinich@mail.ru>
2023-06-16 21:17:49 +0300

    Спринт 10 фиксы №8

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmComparator.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmComparator.java
@@ -9,10 +9,8 @@ public class FilmComparator implements Comparator<Film> {
     public int compare(Film film1,Film film2) {
         if (film1.getLikes().size() > film2.getLikes().size()) {
             return -1;
-        }
-        else if (film1.getLikes().size() < film2.getLikes().size()) {
+        } else if (film1.getLikes().size() < film2.getLikes().size()) {
             return 1;
-        }
-        else return 0;
+        } else return 0;
     }
 }
